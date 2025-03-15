702fe7d272df3ab5aadd1744aa606b8c4cb06fa9
Orphee NVE <=>
2025-03-15 06:23:46 -0400

    Format emotion detector result

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 """ Emotion detection module """
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -9,5 +10,25 @@ def emotion_detector(text_to_analyze):
     body =  { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, headers=headers, json=body, timeout=20)
-    
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    emotions_scores = list(emotions.values())
+    emotions_names = list(emotions.keys())
+
+    anger_score = emotions["anger"]
+    disgust_score = emotions["disgust"]
+    fear_score = emotions["fear"]
+    joy_score = emotions["joy"]
+    sadness_score = emotions["sadness"]
+    dominant_emotion_name = emotions_names[emotions_scores.index(max(emotions_scores))]
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion_name
+    }
+
